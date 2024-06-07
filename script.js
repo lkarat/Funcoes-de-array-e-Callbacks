d09e8cafbc215618acdee72d5e7dd58f389fb47c
Luiz Karat <luiz.karat.silva@gmail.com>
2024-06-07 08:55:04 -0300

    prática 1

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-let idade = 6
+const multiplicaPorTres = (listaNumeros) => {
+
+    let numerosPorTres = []
+    for (const cadaItem of listaNumeros) {
+        numerosPorTres.push(cadaItem * 3)
+
+    }
+    return numerosPorTres
+}
+
+
+
+
+const retornaPares = (lista2Numeros) => {
+    let novoArray = []
+
+    for (const cadaItem2 of lista2Numeros) {
+        if (cadaItem2 % 2 === 0) {
+            novoArray.push(cadaItem2)
+        }
+    }
+    return novoArray
+}
+console.log(retornaPares([1,2,3,4,5,6,7,8]));
+
+
+
+
